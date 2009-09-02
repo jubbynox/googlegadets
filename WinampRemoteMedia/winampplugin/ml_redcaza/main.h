ecfa02c3f259a8aa51a45b0fbe4177c61f122f35
jubbynox <jubbynox@9a519766-8835-0410-a643-67b8b61f6624>
2009-09-02 20:39:02 +0000

    Tagged

--- WinampRemoteMedia/winampplugin/ml_redcaza/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef NULLSOFT_MAINH
-#define NULLSOFT_MAINH
-#include <windows.h>
-
-// Define the web media media library plugin.
-#include "../gen_ml/ml.h"
-extern winampMediaLibraryPlugin WebMediaML;
-
-#endif
