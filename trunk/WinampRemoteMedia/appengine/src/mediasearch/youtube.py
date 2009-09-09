80c23262bfc155311e68b5c25a1961d16de519dc
jubbynox <jubbynox@9a519766-8835-0410-a643-67b8b61f6624>
2009-09-09 11:55:45 +0000

    Improved handling of video URL detection.

--- trunk/WinampRemoteMedia/appengine/src/mediasearch/youtube.py
+++ trunk/WinampRemoteMedia/appengine/src/mediasearch/youtube.py
@@ -1,12 +1,11 @@
 import re
 import logging
+import urllib
 
 from google.appengine.api import urlfetch
 
-GET_VIDEO_INFO_URL = "http://www.youtube.com/get_video_info?&video_id=VIDEO_ID"
-GET_VIDEO_URL = "http://www.youtube.com/get_video?video_id=VIDEO_ID&t=TOKEN_ID&fmt=FMT"
-
-RE_TOKEN_ID_SEARCH = "&token=(.*?)&"
+GET_VIDEO_INFO_URL = "http://www.youtube.com/watch?v=VIDEO_ID"
+RE_FMT_URL_MAP = '"fmt_url_map":.*?"(.*?)"'
 
 class YouTube:
     """YouTube search object."""
@@ -21,20 +20,24 @@ class YouTube:
         
     def constructVideoURL(self, videoID, fmt):
         """Constructs the video URL."""
-        # Get the token.
-        tokenID = self.__getTokenID(videoID)
-        if not tokenID:
+        
+        # Get the raw FMT URL map.
+        rawFmtUrlMap = self.__getRawFmtUrlMap(videoID)
+        if not rawFmtUrlMap:
             # Failure
             return
         
-        # Find the actual download URL.
-        self.getVideoURL = self.__getVideoDownloadURL(videoID, tokenID, fmt)
+        # Convert to something more useful.
+        fmtUrlMap = self.__convertRawMapToFull(rawFmtUrlMap)
+        
+        # Select appropriate URL.
+        self.getVideoURL = self.__selectAppropriateUrl(fmtUrlMap, fmt)
         
         # Success
         return True
     
-    def __getTokenID(self, videoID):
-        """Gets the video token from a supplied video ID."""
+    def __getRawFmtUrlMap(self, videoID):
+        """Gets the raw FMT URL map from the video ID."""
         # Construct URL.
         url = GET_VIDEO_INFO_URL.replace('VIDEO_ID', videoID)
         
@@ -49,26 +52,32 @@ class YouTube:
         # Ensure the content is in the right format.
         result.content = result.content.decode('utf-8', 'ignore')
         
-        # Retrieve the token ID.
-        match = re.search(RE_TOKEN_ID_SEARCH, result.content, re.I)
+        # Retrieve the FMT URL map..
+        match = re.search(RE_FMT_URL_MAP, result.content, re.I)
         if match:
-            return match.group(1)
-        else:
-            return
+            return urllib.unquote_plus(match.group(1))
+        logging.error("Unable to get information from YouTube.")
         
-    def __getVideoDownloadURL(self, videoID, tokenID, fmt):
-        """Gets the actual download URL of the video."""
-        url = GET_VIDEO_URL.replace('VIDEO_ID', videoID).replace('TOKEN_ID', tokenID).replace('FMT', fmt)
-        result = self.__urlFetch.fetch(url, None, urlfetch.GET, {}, True, False)
-        if result.status_code != 303:
-            # Website did not respond as expected, try normal quality
-            url = GET_VIDEO_URL.replace('VIDEO_ID', videoID).replace('TOKEN_ID', tokenID).replace('&fmt=FMT', '')
-            result = self.__urlFetch.fetch(url, None, urlfetch.GET, {}, True, False)
-                
-            if result.status_code != 303:
-                # Something is not working.
-                logging.error('Could not get video source information from site: ' + url + ' (' + str(result.status_code) + ')')
-                logging.debug('result.content = ' + result.content)
-                return
+    def __convertRawMapToFull(self, rawFmtUrlMap):
+        """Converts the raw FMT URL map into something that is more manageable."""
+        # Split on ","
+        fmtUrlPairs = rawFmtUrlMap.split(',')
+        
+        # Create map of FMT against URL.
+        fmtUrlMap = {}
+        for fmtUrlPair in fmtUrlPairs:
+            tmp = fmtUrlPair.split('|')
+            fmtUrlMap[tmp[0]] = tmp[1]
             
-        return result.headers["location"].decode('utf-8', 'ignore')
+        return fmtUrlMap
+    
+    def __selectAppropriateUrl(self, fmtUrlMap, fmt):
+        """Selects the appropriate URL for the supplied format."""
+        # Search for specified format.
+        if fmt in fmtUrlMap:
+            logging.debug("Found format: " + fmt)
+            return fmtUrlMap[fmt]
+        elif '5' in fmtUrlMap:
+            logging.debug("Using default format.")
+            return fmtUrlMap['5']   # Default.
+        logging.debug("No matching formats.")
